93a1ce3e565f32a7c3983c14a59a844f69b2bc76
Andrii Adamchuk <ialinterexa@gmail.com>
2021-08-23 16:47:53 +0200

    git2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Text
+      title="hi, from titel.test"
+      text="hi from text.test"
+      is-published="false"
+    >
+    </Text>
+
+    <User :name="user.name" :age="user.age" :email="user.email" />
   </div>
 </template>
+
+<script>
+import Text from '@/components/Test.vue';
+import User from '@/components/User.vue';
+
+export default {
+  name: 'About',
+  data() {
+    return {
+    user: {
+      name: 'andrii',
+      age: 41,
+      email: 'andrii@q.a',
+    },
+    api: null,
+  }
+},
+
+  components: {
+    Text,
+    User,
+  },
+};
+</script>
